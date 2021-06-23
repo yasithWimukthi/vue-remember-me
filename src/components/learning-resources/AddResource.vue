a14263ce1e98eaa97eb2a1ba9fdfc784dcc4c6e8
yasithWimukthi <it19966922@my.sliit.lk>
2021-06-23 09:45:46 +0530

    implement AddResource component

--- src/components/learning-resources/AddResource.vue
+++ src/components/learning-resources/AddResource.vue
@@ -1,5 +1,23 @@
 <template>
-    
+    <base-card>
+        <form>
+            <div class="form-control">
+                <label for="title">Title : </label>
+                <input id="title" type="text" />
+            </div>
+            <div class="form-control">
+                <label for="description">Description : </label>
+                <textarea id="description" name="description" rows="3" ></textarea>
+            </div>
+            <div class="form-control">
+                <label for="link">Link : </label>
+                <input id="link" type="url" />
+            </div>
+            <div>
+                <base-button type="submit">Add Resource</base-button>
+            </div>
+        </form>
+    </base-card>
 </template>
 
 <script>
@@ -9,5 +27,29 @@
 </script>
 
 <style scoped>
+    label {
+        font-weight: bold;
+        display: block;
+        margin-bottom: 0.5rem;
+    }
 
+    input,
+    textarea {
+        display: block;
+        width: 100%;
+        font: inherit;
+        padding: 0.15rem;
+        border: 1px solid #ccc;
+    }
+
+    input:focus,
+    textarea:focus {
+        outline: none;
+        border-color: #3a0061;
+        background-color: #f7ebff;
+    }
+
+    .form-control {
+        margin: 1rem 0;
+    }
 </style>
