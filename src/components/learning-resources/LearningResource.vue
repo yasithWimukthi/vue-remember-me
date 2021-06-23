c561fa742eb680879ca0ee47bf71979776a13ed1
yasithWimukthi <it19966922@my.sliit.lk>
2021-06-23 08:51:52 +0530

    implement Button component

--- src/components/learning-resources/LearningResource.vue
+++ src/components/learning-resources/LearningResource.vue
@@ -3,7 +3,7 @@
         <base-card>
             <header>
                 <h3>{{title}}</h3>
-                <button>Delete</button>
+                <base-button mode="flat">Delete</base-button>
             </header>
 
             <p>{{description}}</p>
@@ -15,7 +15,9 @@
 </template>
 
 <script>
+  import BaseButton from '../UI/BaseButton';
   export default {
+    components: { BaseButton },
     props:['title','description','link'],
     name: 'LearningResource'
   };
--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue';
 
 import App from './App';
 import BaseCard from './components/UI/BaseCard';
+import BaseButton from './components/UI/BaseButton';
 
 const app = createApp(App);
 
 app.component('base-card',BaseCard);
+app.component('base-button',BaseButton);
 
 app.mount('#app');
