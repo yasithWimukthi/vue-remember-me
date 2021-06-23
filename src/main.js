18967ee4639a4c5b883a98b94c2a29bca7fc8f63
yasithWimukthi <it19966922@my.sliit.lk>
2021-06-23 08:26:23 +0530

    implement BaseCard component

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue';
 
 import App from './App';
+import BaseCard from './components/UI/BaseCard';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+
+app.component('base-card',BaseCard);
+
+app.mount('#app');
